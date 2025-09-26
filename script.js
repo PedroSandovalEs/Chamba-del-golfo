5f71b3b48869eeb579484db192d1d37e3c1840b3
PedroSandovalEs <al06040464@udelg.unigolfo.edu.mx>
2025-09-26 01:13:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Declaramos un arreglo (array) con varias frases motivacionales
+
 const citas = [
   "Bulbasor.",
   "Charmander.",
@@ -24,20 +24,16 @@ const citas = [
   
 ];
 
-// Definimos la función que mostrará una cita aleatoria
-function generarCita() {
-  // Generamos un número aleatorio entre 0 y la cantidad de frases (citas.length)
-  // Math.random() → número entre 0 y 1
-  // * citas.length → escala ese número al rango de frases disponibles
-  // Math.floor() → redondea hacia abajo para obtener un número entero válido
-  const indice = Math.floor(Math.random() * citas.length);
 
-  // Buscamos en el documento el elemento con id="quote"
-  // Cambiamos su contenido de texto por la frase que corresponde al índice generado
+function generarCita() {
+
+
+
   document.getElementById("quote").innerText = citas[indice];
 }
 
 function saludar() {
-alert ("hola, bienvenido al generador de pókemones.");
+alert ("hola, bienvenido al generador de pókemones.");//Aquí añadí mi mensaje personalizado
 }
+
 
